bcbdebd9ac70f1ed8cc44d9f2c78b8f2b695c7e8
mensreaMC <mensreaMC@gmail.com>
2014-04-30 18:37:08 -0400

    -bio blocks and items

---
+++ common/dxm/blocks/EinkornOne.java
@@ -0,0 +1,31 @@
+package dxm.blocks;
+
+import dxm.DeusXMachina;
+import dxm.items.ModItems;
+import dxm.utils.library.Archive;
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+import net.minecraft.item.ItemStack;
+import net.minecraft.world.World;
+
+import java.util.ArrayList;
+
+public class EinkornOne extends Block {
+
+    public EinkornOne() {
+        super(Material.ground);
+        setHardness(1.0F);
+        setResistance(1);
+        setStepSound(Block.soundTypeStone);
+        setBlockName("einkornOne");
+        setCreativeTab(DeusXMachina.instance.tab);
+        setBlockTextureName(Archive.MOD_ID + ":bio/einkornOne");
+    }
+
+    @Override
+    public ArrayList<ItemStack> getDrops(World world, int x, int y, int z, int metadata, int fortune) {
+        ArrayList<ItemStack> ret = new ArrayList<ItemStack>();
+        ret.add(new ItemStack(ModItems.einkornBundle));
+        return ret;
+    }
+}
---
+++ common/dxm/blocks/EinkornZero.java
@@ -0,0 +1,31 @@
+package dxm.blocks;
+
+import dxm.DeusXMachina;
+import dxm.items.ModItems;
+import dxm.utils.library.Archive;
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+import net.minecraft.item.ItemStack;
+import net.minecraft.world.World;
+
+import java.util.ArrayList;
+
+public class EinkornZero extends Block {
+
+    public EinkornZero() {
+        super(Material.ground);
+        setHardness(1.0F);
+        setResistance(1);
+        setStepSound(Block.soundTypeStone);
+        setBlockName("einkornZero");
+        setCreativeTab(DeusXMachina.instance.tab);
+        setBlockTextureName(Archive.MOD_ID + ":bio/einkornZero");
+    }
+
+    @Override
+    public ArrayList<ItemStack> getDrops(World world, int x, int y, int z, int metadata, int fortune) {
+        ArrayList<ItemStack> ret = new ArrayList<ItemStack>();
+        ret.add(new ItemStack(ModItems.einkornBundle));
+        return ret;
+    }
+}
