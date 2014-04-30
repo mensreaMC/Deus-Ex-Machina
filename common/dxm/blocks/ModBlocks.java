7c1242f3e4ce04b56bcaed3a94bc1d7651985130
REDEMPTION <REDEMPTION@192.168.1.101>
2014-04-30 18:36:43 -0400

    -bio blocks and items

--- common/dxm/blocks/ModBlocks.java
+++ common/dxm/blocks/ModBlocks.java
@@ -9,19 +9,21 @@ import net.minecraft.block.material.Material;
 
 public class ModBlocks {
 
-    public static Block aestivumZero;
-    public static Block aestivumOne;
-    public static Block aestivumTwo;
-    public static Block aestivumThree;
-    public static Block asphaltBlock;
     public static Block adminConcreteBlock;
     public static Block adminAsphaltBlock;
+    public static Block asphaltBlock;
     public static Block concreteBlock;
     public static Block concreteCobble;
     public static Block cottonZero;
     public static Block cottonOne;
     public static Block cottonTwo;
     public static Block cottonThree;
+	public static Block einkornZero;
+    public static Block einkornOne;
+    public static Block einkornTwo;
+    public static Block einkornThree;
+    public static Block enormousLeaf;
+    public static Block enormousLog;
     public static Block fertilizedBlock;
     public static Block flaxZero;
     public static Block flaxOne;
@@ -55,42 +57,84 @@ public class ModBlocks {
     }
 
     private static void createBlocks() {
-        adminAsphaltBlock = new ModBlock(Material.ground).setBlockName("adminAsphaltBlock").setBlockTextureName(Archive.MOD_ID + ":fab/asphaltBlock").setHardness(999).setResistance(999).setStepSound(Block.soundTypeStone).setCreativeTab(DeusXMachina.instance.tab);
+        adminAsphaltBlock = new ModBlock(Material.ground).setBlockName("adminAsphaltBlock").setBlockTextureName(Archive.MOD_ID + ":fab/asphaltBlock").setHardness(-1).setResistance(1000).setStepSound(Block.soundTypeStone).setCreativeTab(DeusXMachina.instance.tab);
+        adminConcreteBlock = new ModBlock(Material.ground).setBlockName("adminConcreteBlock").setBlockTextureName(Archive.MOD_ID + ":fab/concreteBlock").setHardness(-1).setResistance(1000).setStepSound(Block.soundTypeStone).setCreativeTab(DeusXMachina.instance.tab);
         asphaltBlock = new ModBlock(Material.ground).setBlockName("asphaltBlock").setBlockTextureName(Archive.MOD_ID + ":fab/asphaltBlock").setHardness(1).setResistance(10).setStepSound(Block.soundTypeStone).setCreativeTab(DeusXMachina.instance.tab);
-        adminConcreteBlock = new ModBlock(Material.ground).setBlockName("adminConcreteBlock").setBlockTextureName(Archive.MOD_ID + ":fab/concreteBlock").setHardness(999).setResistance(999).setStepSound(Block.soundTypeStone).setCreativeTab(DeusXMachina.instance.tab);
         concreteBlock = new ConcreteBlock();
-        concreteCobble = new ModBlock(Material.ground).setBlockName("concreteCobble").setBlockTextureName(Archive.MOD_ID + ":fab/concreteCobble").setHardness(4).setResistance(10).setStepSound(Block.soundTypeStone).setCreativeTab(DeusXMachina.instance.tab);
+		concreteCobble = new ModBlock(Material.ground).setBlockName("concreteCobble").setBlockTextureName(Archive.MOD_ID + ":fab/concreteCobble").setHardness(4).setResistance(10).setStepSound(Block.soundTypeStone).setCreativeTab(DeusXMachina.instance.tab);
+        cottonZero = new CottonZero();
+        cottonOne = new CottonOne();
+        cottonTwo = new CottonTwo();
+        cottonThree = new CottonThree();
+		einkornZero = new EinkornZero();
+        einkornOne = new EinkornOne();
+        einkornTwo = new EinkornTwo();
+        einkornThree = new EinkornThree();
+        enormousLeaf = new ModBlock(Material.ground).setBlockName("enormousLeaf").setBlockTextureName(Archive.MOD_ID + ":bio/enormousLeaf").setHardness(1).setResistance(1).setStepSound(Block.soundTypeGrass).setCreativeTab(DeusXMachina.instance.tab);
+        enormousLog = new ModBlock(Material.ground).setBlockName("enormousLog").setBlockTextureName(Archive.MOD_ID + ":bio/enormousLog").setHardness(1).setResistance(1).setStepSound(Block.soundTypeWood).setCreativeTab(DeusXMachina.instance.tab);
         fertilizedBlock = new ModBlock(Material.ground).setBlockName("fertilizedBlock").setBlockTextureName(Archive.MOD_ID + ":bio/fertilizedBlock").setHardness(4).setResistance(10).setStepSound(Block.soundTypeGrass).setCreativeTab(DeusXMachina.instance.tab);
-        geoActive = new ModBlockTopTexture(Material.ground).setBlockName("geoActive").setBlockTextureName(Archive.MOD_ID + ":geo/geoActive").setHardness(999).setResistance(999).setStepSound(Block.soundTypeStone).setCreativeTab(DeusXMachina.instance.tab);
-        geoGas = new ModBlockTopTexture(Material.ground).setBlockName("geoGas").setBlockTextureName(Archive.MOD_ID + ":geo/geoGas").setHardness(999).setResistance(999).setStepSound(Block.soundTypeStone).setCreativeTab(DeusXMachina.instance.tab);
-        geoHeavy = new ModBlockTopTexture(Material.ground).setBlockName("geoHeavy").setBlockTextureName(Archive.MOD_ID + ":geo/geoHeavy").setHardness(999).setResistance(999).setStepSound(Block.soundTypeStone).setCreativeTab(DeusXMachina.instance.tab);
-        geoLight = new ModBlockTopTexture(Material.ground).setBlockName("geoLight").setBlockTextureName(Archive.MOD_ID + ":geo/geoLight").setHardness(999).setResistance(999).setStepSound(Block.soundTypeStone).setCreativeTab(DeusXMachina.instance.tab);
-        geoShale = new ModBlock(Material.ground).setBlockName("geoShale").setBlockTextureName(Archive.MOD_ID + ":geo/geoShale").setHardness(999).setResistance(999).setStepSound(Block.soundTypeStone).setCreativeTab(DeusXMachina.instance.tab);
-        monaziteSand = new BlockFalling().setBlockName("monaziteSand").setBlockTextureName(Archive.MOD_ID + ":ore/monaziteSand").setHardness(4).setStepSound(Block.soundTypeSand).setCreativeTab(DeusXMachina.instance.tab);
+        flaxZero = new FlaxZero();
+        flaxOne = new FlaxOne();
+        flaxTwo = new FlaxTwo();
+        flaxThree = new FlaxThree();
+        geoActive = new ModBlockTopTexture(Material.ground).setBlockName("geoActive").setBlockTextureName(Archive.MOD_ID + ":geo/geoActive").setHardness(-1).setResistance(1000).setStepSound(Block.soundTypeStone).setCreativeTab(DeusXMachina.instance.tab);
+        geoGas = new ModBlockTopTexture(Material.ground).setBlockName("geoGas").setBlockTextureName(Archive.MOD_ID + ":geo/geoGas").setHardness(-1).setResistance(-1).setStepSound(Block.soundTypeStone).setCreativeTab(DeusXMachina.instance.tab);
+        geoHeavy = new ModBlockTopTexture(Material.ground).setBlockName("geoHeavy").setBlockTextureName(Archive.MOD_ID + ":geo/geoHeavy").setHardness(-1).setResistance(1000).setStepSound(Block.soundTypeStone).setCreativeTab(DeusXMachina.instance.tab);
+        geoLight = new ModBlockTopTexture(Material.ground).setBlockName("geoLight").setBlockTextureName(Archive.MOD_ID + ":geo/geoLight").setHardness(-1).setResistance(1000).setStepSound(Block.soundTypeStone).setCreativeTab(DeusXMachina.instance.tab);
+        geoShale = new ModBlock(Material.ground).setBlockName("geoShale").setBlockTextureName(Archive.MOD_ID + ":geo/geoShale").setHardness(-1).setResistance(1000).setStepSound(Block.soundTypeStone).setCreativeTab(DeusXMachina.instance.tab);
+        hempZero = new HempZero();
+        hempOne = new HempOne();
+        hempTwo = new HempTwo();
+        hempThree = new HempThree();
+		monaziteSand = new BlockFalling().setBlockName("monaziteSand").setBlockTextureName(Archive.MOD_ID + ":ore/monaziteSand").setHardness(4).setStepSound(Block.soundTypeSand).setCreativeTab(DeusXMachina.instance.tab);
         mudBrick = new ModBlock(Material.ground).setBlockName("mudBrick").setBlockTextureName(Archive.MOD_ID + ":fab/mudBrick").setHardness(1).setResistance(2).setStepSound(Block.soundTypeStone).setCreativeTab(DeusXMachina.instance.tab);
         reConcreteBlock = new ReConcreteBlock();
         rubberLeaf = new ModBlock(Material.ground).setBlockName("rubberLeaf").setBlockTextureName(Archive.MOD_ID + ":bio/rubberLeaf").setHardness(1).setResistance(1).setStepSound(Block.soundTypeGrass).setCreativeTab(DeusXMachina.instance.tab);
-        rubberLog = new RubberLog();
+        rubberLog = new ModBlock(Material.ground).setBlockName("rubberLog").setBlockTextureName(Archive.MOD_ID + ":bio/rubberLog").setHardness(1).setResistance(1).setStepSound(Block.soundTypeWood).setCreativeTab(DeusXMachina.instance.tab);
+		sugarCane = new SugarCane();
+        switchgrassZero = new SwitchgrassZero();
+        switchgrassOne = new SwitchgrassOne();
         tilledFertilizedBlock = new TilledFertilizedBlock();
     }
 
     private static void registerBlocks() {
         MinecraftHelper.registerBlock(adminAsphaltBlock);
-        MinecraftHelper.registerBlock(asphaltBlock);
         MinecraftHelper.registerBlock(adminConcreteBlock);
-        MinecraftHelper.registerBlock(concreteCobble);
+        MinecraftHelper.registerBlock(asphaltBlock);
         MinecraftHelper.registerBlock(concreteBlock);
+        MinecraftHelper.registerBlock(concreteCobble);
+        MinecraftHelper.registerBlock(cottonZero);
+        MinecraftHelper.registerBlock(cottonOne);
+        MinecraftHelper.registerBlock(cottonTwo);
+        MinecraftHelper.registerBlock(cottonThree);
+		MinecraftHelper.registerBlock(einkornZero);
+        MinecraftHelper.registerBlock(einkornOne);
+        MinecraftHelper.registerBlock(einkornTwo);
+        MinecraftHelper.registerBlock(einkornThree);
+        MinecraftHelper.registerBlock(enormousLeaf);
+        MinecraftHelper.registerBlock(enormousLog);
         MinecraftHelper.registerBlock(fertilizedBlock);
+        MinecraftHelper.registerBlock(flaxZero);
+        MinecraftHelper.registerBlock(flaxOne);
+        MinecraftHelper.registerBlock(flaxTwo);
+        MinecraftHelper.registerBlock(flaxThree);
         MinecraftHelper.registerBlock(monaziteSand);
         MinecraftHelper.registerBlock(geoActive);
         MinecraftHelper.registerBlock(geoGas);
         MinecraftHelper.registerBlock(geoHeavy);
         MinecraftHelper.registerBlock(geoLight);
         MinecraftHelper.registerBlock(geoShale);
+        MinecraftHelper.registerBlock(hempZero);
+        MinecraftHelper.registerBlock(hempOne);
+        MinecraftHelper.registerBlock(hempTwo);
+        MinecraftHelper.registerBlock(hempThree);
         MinecraftHelper.registerBlock(mudBrick);
         MinecraftHelper.registerBlock(reConcreteBlock);
         MinecraftHelper.registerBlock(rubberLeaf);
         MinecraftHelper.registerBlock(rubberLog);
+        MinecraftHelper.registerBlock(sugarCane);
+        MinecraftHelper.registerBlock(switchgrassZero);
+        MinecraftHelper.registerBlock(switchgrassOne);
         MinecraftHelper.registerBlock(tilledFertilizedBlock);
     }
 }
--- common/dxm/items/ModItems.java
+++ common/dxm/items/ModItems.java
@@ -3,27 +3,57 @@ package dxm.items;
 import dxm.DeusXMachina;
 import dxm.utils.MinecraftHelper;
 import dxm.utils.library.Archive;
+import net.minecraft.block.Block;
 import net.minecraft.item.Item;
 
 public class ModItems {
 
-    public static Item squee;
     public static Item rebar;
     public static Item cottonSeed;
     public static Item cottonBole;
+    public static Item cottonBundle;
     public static Item hempSeed;
-    public static Item hemp;
+    public static Item hempBundle;
     public static Item switchgrassSeed;
-    public static Item switchgrass;
-    public static Item aestivumSeed;
+    public static Item switchgrassBundle;
+    public static Item einkornSeed;
+    public static Item einkornBundle;
     public static Item rubberSapling;
     public static Item flaxSeeds;
+    public static Item flaxBundle;
+	public static Item sugarcaneBundle;
     
     public static void setup() {
-        squee = new Item().setUnlocalizedName("squee").setTextureName(Archive.MOD_ID + ":squee").setCreativeTab(DeusXMachina.instance.tab);
-        rebar = new Item().setUnlocalizedName("rebar").setTextureName(Archive.MOD_ID + ":rebar").setCreativeTab(DeusXMachina.instance.tab);
+   
+    	rebar = new Item().setUnlocalizedName("rebar").setTextureName(Archive.MOD_ID + ":rebar").setCreativeTab(DeusXMachina.instance.tab);
+    	cottonSeed = new Item().setUnlocalizedName("cottonSeeds").setTextureName(Archive.MOD_ID + ":cottonSeeds").setCreativeTab(DeusXMachina.instance.tab);
+    	cottonBole = new Item().setUnlocalizedName("cottonBole").setTextureName(Archive.MOD_ID + ":cottonBole").setCreativeTab(DeusXMachina.instance.tab);
+    	cottonBundle = new Item().setUnlocalizedName("cottonBole").setTextureName(Archive.MOD_ID + ":cottonBundle").setCreativeTab(DeusXMachina.instance.tab);
+    	hempSeed = new Item().setUnlocalizedName("hempSeeds").setTextureName(Archive.MOD_ID + ":hempSeeds").setCreativeTab(DeusXMachina.instance.tab);
+    	hempBundle = new Item().setUnlocalizedName("hemp").setTextureName(Archive.MOD_ID + ":hempBundle").setCreativeTab(DeusXMachina.instance.tab);
+    	sugarcaneBundle = new Item().setUnlocalizedName("sugarcaneBundle").setTextureName(Archive.MOD_ID + ":sugarcaneBundle").setCreativeTab(DeusXMachina.instance.tab);
+    	switchgrassSeed = new Item().setUnlocalizedName("switchgrassSeeds").setTextureName(Archive.MOD_ID + ":switchgrassSeeds").setCreativeTab(DeusXMachina.instance.tab);
+    	switchgrassBundle = new Item().setUnlocalizedName("switchgrass").setTextureName(Archive.MOD_ID + ":switchgrassBundle").setCreativeTab(DeusXMachina.instance.tab);
+    	einkornSeed = new Item().setUnlocalizedName("aestivumSeeds").setTextureName(Archive.MOD_ID + ":einkornSeeds").setCreativeTab(DeusXMachina.instance.tab);
+    	einkornBundle = new Item().setUnlocalizedName("aestivum").setTextureName(Archive.MOD_ID + ":einkornBundle").setCreativeTab(DeusXMachina.instance.tab);
+    	rubberSapling = new Item().setUnlocalizedName("rubberSapling").setTextureName(Archive.MOD_ID + ":rubberSapling").setCreativeTab(DeusXMachina.instance.tab);
+    	flaxSeeds = new Item().setUnlocalizedName("flaxSeeds").setTextureName(Archive.MOD_ID + ":flaxSeeds").setCreativeTab(DeusXMachina.instance.tab);
+    	flaxBundle = new Item().setUnlocalizedName("flax").setTextureName(Archive.MOD_ID + ":flaxBundle").setCreativeTab(DeusXMachina.instance.tab);
+    	
+    	MinecraftHelper.registerItem(rebar);
+        MinecraftHelper.registerItem(cottonSeed);
+        MinecraftHelper.registerItem(cottonBole);
+        MinecraftHelper.registerItem(cottonBundle);
+        MinecraftHelper.registerItem(hempSeed);
+        MinecraftHelper.registerItem(hempBundle);
+        MinecraftHelper.registerItem(sugarcaneBundle);
+        MinecraftHelper.registerItem(switchgrassSeed);
+        MinecraftHelper.registerItem(switchgrassBundle);
+        MinecraftHelper.registerItem(einkornSeed);
+        MinecraftHelper.registerItem(einkornBundle);
+        MinecraftHelper.registerItem(rubberSapling);
+        MinecraftHelper.registerItem(flaxSeeds);
+        MinecraftHelper.registerItem(flaxBundle);
 
-        MinecraftHelper.registerItem(squee);
-        MinecraftHelper.registerItem(rebar);
     }
 }
