a4e152ade957c24aaffae1ef70cbd4d2a696fb05
captain_shadows <captain_shadows@aol.com>
2014-04-30 21:21:47 -0400

    more cleaning up

--- common/dxm/blocks/HempCrop.java
+++ common/dxm/blocks/HempCrop.java
@@ -8,12 +8,13 @@ package dxm.blocks;
 
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
+import dxm.DeusXMachina;
+import dxm.utils.StringHelper;
 import dxm.utils.library.Archive;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockBreakable;
 import net.minecraft.block.BlockCrops;
 import net.minecraft.client.renderer.texture.IIconRegister;
-import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemSeeds;
 import net.minecraft.item.ItemStack;
@@ -29,21 +30,14 @@ public class HempCrop extends BlockCrops {
     protected Item myProduct;
     private int fertileGrowthRate;
     private int nonFertileGrowthRate;
-    private int myTexIndex;
-    public static int blockID;
 
-    public HempCrop(int id, int texIndex, ItemSeeds seed, Item product, int fertGrowth, int nonFertGrowth, String name) {
-        // super(id, texIndex);
-        super();
+    public HempCrop(ItemSeeds seed, Item product, int fertGrowth, int nonFertGrowth, String name) {
         this.mySeed = seed;
         this.myProduct = product;
         this.fertileGrowthRate = fertGrowth;
         this.nonFertileGrowthRate = nonFertGrowth;
-        this.myTexIndex = texIndex;
         setTickRandomly(true);
-        // setRequiresSelfNotify(); XXX
-        // setTextureFile(DrugMain.tex);
-        setCreativeTab(CreativeTabs.tabMisc);
+        setCreativeTab(DeusXMachina.instance.tab);
         setBlockName("hempCrop");
         setBlockBounds(0.0F, 0.0F, 0.0F, 1.0F, 0.2F, 1.0F);
     }
@@ -52,8 +46,7 @@ public class HempCrop extends BlockCrops {
         if(world.getBlockMetadata(x, y, z) < 3) {
             if(canSeeSky(world, x, y, z)) {
                 //
-                world.setBlockMetadataWithNotify(x, y, z,
-                        world.getBlockMetadata(x, y, z) + 1, 3);
+                world.setBlockMetadataWithNotify(x, y, z, world.getBlockMetadata(x, y, z) + 1, 3);
                 setBlockBounds(0.0F, 0.0F, 0.0F, 1.0F, 0.2F + world.getBlockMetadata(x, y, z) * 0.2F, 1.0F);
             }
         }
@@ -116,23 +109,19 @@ public class HempCrop extends BlockCrops {
     }
 
     @SideOnly(Side.CLIENT)
-    private IIcon[] icons;
+    private IIcon[] icons = new IIcon[4];
 
     @SideOnly(Side.CLIENT)
-    public void registerIcons(IIconRegister par1IconRegister) {
-        icons = new IIcon[4];
-
+    public void registerBlockIcons(IIconRegister register) {
         for(int i = 0; i < this.icons.length; i++) {
-            this.icons[i] = par1IconRegister.registerIcon(Archive.MOD_ID + ":" + (this.getUnlocalizedName().substring(5)) + "_" + i);
+            icons[i] = register.registerIcon(Archive.MOD_ID + ":" + StringHelper.cleanUnlocalizedBlockName(this) + "_" + i);
         }
     }
-
-
-    @SideOnly(Side.CLIENT)
 
     /**
      * From the specified side and block metadata retrieves the blocks texture. Args: side, metadata
      */
+    @SideOnly(Side.CLIENT)
     public IIcon getIcon(int par1, int par2) {
         if(par2 < 0 || par2 > 3) {
             par2 = 3;
